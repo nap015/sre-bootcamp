bc6a4db96aab49197a28d48cabac02ef28dfde22
Nayoung Park <skdud712@gmail.com>
2022-09-03 23:48:46 -0700

    backup

--- auth_api/python/methods.py
+++ auth_api/python/methods.py
@@ -1,8 +1,20 @@
 # These functions need to be implemented
+import hashlib
+
 class Token:
+    
+    #import mysql.connect
 
     def generate_token(self, username, password):
-        return 'test'
+        string = 'F^S%QljSfV'+password
+        r = hashlib.sha512(string.encode()).hexdigest()
+        return r
+        
+#         cnx = mysql.connector.connect(user='secret', password='noPow3r',
+#                               host='127.0.0.1',
+#                               database='employees')
+#         cnx.close()
+        #return 'test'
 
 
 class Restricted:
